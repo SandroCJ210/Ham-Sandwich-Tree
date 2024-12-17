65ad0dc1089bc51453e790fd70d31fdcda44e0d2
Max <oscar.torres.m@uni.pe>
2024-12-17 13:20:29 -0500

    Doc: Vector2

--- src/public/Math/Vector2.h
+++ src/public/Math/Vector2.h
@@ -8,11 +8,6 @@ public:
 	double x;
 	double y;
 
-private:
-	double magnitude = 0;
-	double sqrMagnitude = 0;
-	Vector2* normalize = nullptr;
-
 public:
 	static Vector2 zero;
 	static Vector2 one;
@@ -25,18 +20,62 @@ private:
 	static double treshold;
 
 public:
+	/**
+	 * @brief Empty constructor for a new Vector2 object
+	 */
 	Vector2();
-	Vector2(double x);
-	Vector2(double x, double y);
-	Vector2(const Vector2& v);
-	Vector2(const Vector3& v);
-	~Vector2();
 	
-	double Magnitude();
-	double SquaredMagnitude();
-	Vector2& Normalize();
+	/**
+	 * @brief Constructor for a new Vector2 initializing all values to the same value
+	 * @param x The value of all the vector parameters
+	 */
+	Vector2(double x);
+	
+	/**
+	 * Default constructor of the vector
+	 */
+	Vector2(double x, double y);
+	
+	/**
+	 * Initialize a Vector2 copying the values from another Vector2
+	 */
+	Vector2(const Vector2& v);
 
+	/**
+	 * @brief Initializes a Vector2 copying the x and y values from a Vector3
+	 * @param v Vector3 to copy the x and y values
+	 */
+	Vector2(const Vector3& v);
+	
+	/**
+	 * @brief Destructor of the Vector2
+	 */
+	~Vector2();
+
+	
+	/**
+	 * @brief Calculate the magnitude of the vector
+	 */
+	double Magnitude();
+	
+	/**
+	 * @brief Calculates the squared magnitude of the vector
+	 */
+	double SquaredMagnitude();
+	
+	/**
+	 * @brief Normalize the vector
+	 */
+	Vector2 Normalize();
+
+	/**
+	 * @brief calculates the dot product of two vectors
+	 */
 	static double Dot(const Vector2& v, const Vector2& w);
+	
+	/**
+	 * @brief calculates the euclidean distance between two vectors
+	 */
 	static double Distance(const Vector2& v, const Vector2& w);
 	
 	Vector2& operator+=(const Vector2& v);
