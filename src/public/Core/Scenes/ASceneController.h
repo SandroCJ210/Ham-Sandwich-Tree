728362c88d9e413edb3c6daee107a1bc42d1ffa9
Max <oscar.torres.m@uni.pe>
2025-01-12 22:40:18 -0500

    Ascene documentation

--- src/public/Core/Scenes/ASceneController.h
+++ src/public/Core/Scenes/ASceneController.h
@@ -5,24 +5,62 @@
 class ASceneController {
 
 private:
+	// bunch of stuff for calculating the delta time and physics time offset
+	// (this is used for know when to exceute the physics)
 	double current;
 	double previous;
 	double physicsTimeOffset = 0.0;
 	
 public:
+	/**
+	 * @brief The list of objects in the scene
+	 */
 	std::vector<AObject*> objects = std::vector<AObject*>();
 	
 	ASceneController();
 	virtual ~ASceneController();
 
+	/**
+	 * Excecute the scene general update
+	 */
 	virtual void SceneUpdate();
 
+	/**
+	 * @brief This method is called when the scene is created before the start
+	 * This is the first method to be called
+	 */
 	virtual void Awake();
+
+	/**
+	 * @brief This method is called when the scene is created after the awake
+	 */
 	virtual void Start();
+
+	/**
+	 * @brief This method is for the physics update and excecuted in a fixed time
+	 */
 	virtual void FixedUpdate();
+
+	/**
+	 * @brief This method is called every frame
+	 * @param deltaTime The	time between frames
+	 */
 	virtual void Update(double deltaTime);
+
+	/**
+	 * @brief This method is called after the update
+	 * Is used mainly for the rendering
+	 */
 	virtual void LateUpdate();
+
+	/**
+	 * This method is called when the scene is destroyed
+	 */
 	virtual void End();
 
+	/**
+	 * Adds an object to this scene
+	 * @param object The object to add
+	 */
 	void AddObject(AObject* object);
 };
