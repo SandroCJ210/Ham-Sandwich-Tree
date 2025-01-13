22ca1ecfff21c9ca47aed79be3ff6a614696af1a
Max <oscar.torres.m@uni.pe>
2025-01-12 22:30:24 -0500

    Global variables and window documentation

--- src/public/Core/Global.h
+++ src/public/Core/Global.h
@@ -4,15 +4,39 @@
 class Global {
 public:
 	static const float FIXED_DELTA_TIME;
+	/**
+	 * Screen width in pixels
+	 */
 	static const  int SCREEN_WIDTH = 1024;
+	/**
+	 * Screen height in pixels
+	 */
 	static const int SCREEN_HEIGHT = 576;
+	/**
+	 * Screen format (width/height)
+	 */
 	static const float SCREEN_FORMAT;
+	/**
+	 * Multiplier of the meter for the engine global space
+	 */
 	static const  float METER;
 	
 	static const bool DEBUG = false;
+	/**
+	 * The framerate of the game in debug mode (Unused)
+	 */
 	static const float DEBUG_FRAMERATE;
 
+	/**
+	 * Forward vector in global space
+	 */
 	static const glm::vec3 FORWARD;
+	/**
+	 * Right vector in global space
+	 */
 	static const glm::vec3 RIGHT;
+	/**
+	 * Up vector in global space
+	 */
 	static const glm::vec3 UP;
 };
--- src/public/Core/Window.h
+++ src/public/Core/Window.h
@@ -4,6 +4,9 @@
 #include "Util/Singleton.h"
 #include "Core/Scenes/ASceneController.h"
 
+/**
+ * @brief This class represents the window of the game
+ */
 class Window : public Singleton<Window>{
 
 private:
@@ -11,18 +14,49 @@ private:
 
 public:
 	Window();
+	
+	/**
+	 * This method excecute the initialization, loop and end of the game
+	 */
 	void Excecute();
+
+	/**
+	 * @brief This method adds an object to the actual scene
+	 * @param object The object to add
+	 */
 	void AddObjectToScene(AObject* object);
 
 private:
+	/**
+	 * The initialization of the window
+	 */
 	void Start();
+	/**
+	 * The loop of the game
+	 */
 	void Update();
+	/**
+	 * The end of the game
+	 */
 	void End();
+
+	/**
+	 * @brief This method processes the frame of the window
+	 * @param window The window to process
+	 */
 	void ProcessFrame(struct GLFWwindow* window);
 
 public:
+	/**
+	 * @brief This method sets the actual scene to execute
+	 * @param scene	The scene to set
+	 */
 	void SetActualScene(ASceneController* scene) { actualScene = scene; }
-	
+
+	/**
+	 * 
+	 * @return The actual scene
+	 */
 	ASceneController* GetActualScene() const { return actualScene; }
 };
 template class Singleton<Window>;
