53a718c4644cc1418cd7a075c1bc15728aa97864
Max <oscar.torres.m@uni.pe>
2025-01-19 11:49:22 -0500

    Docs: Physics engine documentation

--- src/public/Core/Physics/PhysicsEngine2D.h
+++ src/public/Core/Physics/PhysicsEngine2D.h
@@ -7,16 +7,30 @@ class AObject;
 class ACollider2DComponent;
 class Rigidbody2DComponent;
 
+/**
+ * This class encasulates all the physics calculations for 2D objects.
+ */
 class PhysicsEngine2D {
+	
 public:
+	/**
+	 * This class represents a 2D raycast hit.
+	 */
 	class Raycast2D {
 	public:
 		bool hit = false;
 		float time;
 		glm::vec2 position;
 	};
+	
 private:
+	/**
+	 * A list of all rigidbodies in the scene.
+	 */
 	static std::vector<Rigidbody2DComponent*> rigidbodies;
+	/**
+	 * A list of all colliders in the scene.
+	 */
 	static std::vector<ACollider2DComponent*> colliders;
 
 public:
@@ -24,25 +38,79 @@ public:
 	static void Update();
 
 private:
+	/**
+	 * @brief Adds the rigidbodies and the colliders inside the object to the physics engine.
+	 * @param object The object to search for rigidbodies and colliders.
+	 */
 	static void AddObjectRigidbodyAndCollider(AObject* object);
+	
+	/**
+	 * @brief Adds a rigidbody to the physics engine.
+	 * @param rigidBody The rigidbody to add.
+	 */
 	static void AddRigidbody(Rigidbody2DComponent* rigidBody);
+
+	/**
+	 * @brief Adds a collider to the physics engine.
+	 * @param collider The collider to add.
+	 */
 	static void AddCollider(ACollider2DComponent* collider);
 
+	/**
+	 * @brief Searches for the near colliders to the rigidbody.
+	 * @param rigidbody The rigidbody to get the near colliders.
+	 * @return A list of colliders that are near to the rigidbody.
+	 */
 	static std::vector<ACollider2DComponent*> GetNearColliders(Rigidbody2DComponent* rigidbody);
 	
 public:
+	/**
+	 * @brief Calculate the min and max vertices positions of a square collider
+	 * and stores them in the min and max vectors.
+	 * @param min A reference to the min vector.
+	 * @param max A reference to the max vector.
+	 * @param center The center of the quad.
+	 * @param halfSize The half size of the quad.
+	 */
 	static void CalcMinAndMax(glm::vec2& min, glm::vec2& max, glm::vec2 center, glm::vec2 halfSize);
-	
+
+	/**
+	 * @brief Checks if a point intersects a square collider.
+	 * @param point The point to check.
+	 * @param collider The collider to check.
+	 * @return True if the point intersects the collider, false otherwise.
+	 */
 	static bool PointIntersectsSquareCollider(glm::vec2 point, SquareColliderComponent* collider);
 
+	/**
+	 * @brief Checks if a square collider intersects another square collider.
+	 * @param collider1 The first collider to check.
+	 * @param collider2 The second collider to check.
+	 * @return True if the colliders intersect, false otherwise.
+	 */
 	static bool SquareColliderIntesectsSquareCollider(
 		SquareColliderComponent* collider1,
 		SquareColliderComponent* collider2);
 
+	/**
+	 * @brief Calculates the penetration vector of two square colliders.
+	 * @param collider1 The first collider to check.
+	 * @param collider2 The second collider to check.
+	 * @return The penetration vector of the colliders if they intersect, otherwise (0, 0).
+	 */
 	static glm::vec2 SquareColliderPenetration(
 		SquareColliderComponent* collider1,
 		SquareColliderComponent* collider2);
 
+	/**
+	 * @brief Checks if a raycast intersects a square collider.
+	 * @param position The start position of the raycast.
+	 * @param direction The direction of the raycast.
+	 * @param distance The distance of the raycast.
+	 * @param colliderPosition The position of the collider to check the collision.
+	 * @param colliderHalfSize The half size of the collider to check the collision.
+	 * @return A Raycast2D object with the information of the raycast.
+	 */
 	static Raycast2D RaycastSquareCollider(
 		glm::vec2 position,
 		glm::vec2 direction,
--- src/public/Core/Physics/PhysicsEngine3D.h
+++ src/public/Core/Physics/PhysicsEngine3D.h
@@ -7,8 +7,15 @@ class ACollider3DComponent;
 class BoxColliderComponent;
 class AObject;
 
+/**
+ * This class encasulates all the physics calculations for 3D objects.
+ */
 class PhysicsEngine3D {
+
 public:
+	/**
+	 * This class represents a 3D raycast hit.
+	 */
 	class Raycast3D {
 	public:
 		bool hit = false;
@@ -16,8 +23,15 @@ public:
 		glm::vec3 position;
 	};
 	
-private:
+private:	
+	/**
+	 * A list of all rigidbodies in the scene.
+	 */
 	static std::vector<Rigidbody3DComponent*> rigidbodies;
+
+	/**
+	 * A list of all colliders in the scene.
+	 */
 	static std::vector<ACollider3DComponent*> colliders;
 
 public:
@@ -25,25 +39,77 @@ public:
 	static void Update();
 
 private:
+	/**
+	 * @brief Adds the rigidbodies and the colliders inside the object to the physics engine.
+	 * @param object The object to search for rigidbodies and colliders.
+	 */
 	static void AddObjectRigidbodyAndCollider(AObject* object);
+
+	/**
+	 * @brief Adds a rigidbody to the physics engine.
+	 * @param rigidBody The rigidbody to add.
+	 */
 	static void AddRigidbody(Rigidbody3DComponent* rigidBody);
+	
+	/**
+	 * @brief Adds a collider to the physics engine.
+	 * @param collider The collider to add.
+	 */
 	static void AddCollider(ACollider3DComponent* collider);
 
+	/**
+	 * @brief Searches for the near colliders to the rigidbody.
+	 * @param rigidbody The rigidbody to get the near colliders.
+	 * @return A list of colliders that are near to the rigidbody.
+	 */
 	static std::vector<ACollider3DComponent*> GetNearColliders(Rigidbody3DComponent* rigidbody);
 
 public:
+
+	/**
+	 * @brief Calculates the minimum and maximum points of a box collider.
+	 * @param min The minimum point of the box collider.
+	 * @param max The maximum point of the box collider.
+	 * @param center The center of the box collider.
+	 * @param halfSize The half size of the box collider.
+	 */
 	static void CalcMinAndMax(glm::vec3& min, glm::vec3& max, glm::vec3 center, glm::vec3 halfSize);
 
+	/**
+	 * @brief Checks if a point intersects a box collider.
+	 * @param point The point to check.
+	 * @param collider The collider to check.
+	 * @return True if the point intersects the collider, false otherwise.
+	 */
 	static bool PointIntersectsBoxCollider(glm::vec3 point, BoxColliderComponent* collider);
 
+	/**
+	 * @brief Checks if a box collider intersects another box collider.
+	 * @param collider1 The first collider.
+	 * @param collider2 The second collider.
+	 * @return True if the colliders intersect, false otherwise.
+	 */
 	static bool BoxColliderIntesectsBoxCollider(
 		BoxColliderComponent* collider1,
 		BoxColliderComponent* collider2);
 
+	/**
+	 * @brief Calculates the penetration vector of two box colliders.
+	 * @param collider1 The first collider.
+	 * @param collider2 The second collider.
+	 * @return The penetration vector of the two colliders if they intersect, otherwise (0, 0, 0).
+	 */
 	static glm::vec3 BoxColliderPenetration(
 		BoxColliderComponent* collider1,
 		BoxColliderComponent* collider2);
 
+	/**
+	 * @brief Casts a ray in the scene and returns the first collider that intersects the ray.
+	 * @param position The position of the ray.
+	 * @param direction The direction of the ray.
+	 * @param distance The distance of the ray.
+	 * @return The first collider that intersects the ray.
+	 */
 	static Raycast3D RaycastBoxCollider(
 		glm::vec3 position,
 		glm::vec3 direction,
